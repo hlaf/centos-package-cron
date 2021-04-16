a38d3b442cd805f53592d1e3e30c14ac7772c099
emt-jenkins <jenkinsci@emtegrity.com>
2021-04-16 01:00:46 +0000

    Bump version to 1.0.86.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(TestCommand):
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
-version = '1.0.85'
+version = '1.0.86'
 setuptools.setup(name='centos_package_cron',
       version=version,
       description='CentOS Package Update Utilities',
